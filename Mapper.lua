86087da1f9af977d0f8817f478bbf2b458ea9c37
yaya3636 <65079081+yaya3636@users.noreply.github.com>
2021-10-25 22:18:16 +0200

    Gestion mine

--- Mapper.lua
+++ Mapper.lua
@@ -7,8 +7,89 @@ JSON = dofile(global:getCurrentDirectory() .. "\\YAYA\\Module\\JSON.lua")
 
 local currentDirectory = global:getCurrentScriptDirectory()
 
+local mineurInfo = {
+    Fer = { name = "Fer", gatherId = 17, objectId = 312, jobId = 24, minLvlToFarm = 1 },
+    Cuivre = { name = "Cuivre", gatherId = 53, objectId = 441, jobId = 24, minLvlToFarm = 20 },
+    Bronze = { name = "Bronze", gatherId = 55, objectId = 442, jobId = 24, minLvlToFarm = 40 },
+    Kobalte = { name = "Kobalte", gatherId = 37, objectId = 443, jobId = 24, minLvlToFarm = 60 },
+    Manganese = { name = "Manganèse", gatherId = 54, objectId = 445, jobId = 24, minLvlToFarm = 80 },
+    Etain = { name = "Etain", gatherId = 52, objectId = 444, jobId = 24, minLvlToFarm = 100 },
+    Silicate = { name = "Silicate", gatherId = 114, objectId = 7032, jobId = 24, minLvlToFarm = 100 },
+    Argent = { name = "Argent", gatherId = 24, objectId = 312, jobId = 24, minLvlToFarm = 120 },
+    Bauxite = { name = "Bauxite", gatherId = 26, objectId = 446, jobId = 24, minLvlToFarm = 140 },
+    Or = { name = "Or", gatherId = 25, objectId = 312, jobId = 24, minLvlToFarm = 160 },
+    Dolomite = { name = "Dolomite", gatherId = 113, objectId = 7033, jobId = 24, minLvlToFarm = 180 },
+    Obsidienne = { name = "Obsidienne", gatherId = 135, objectId = 11110, jobId = 24, minLvlToFarm = 200 },
+}
+
+local idMine = 1000
+local inMine = false
+
 function move()
     developer:registerMessage("MapComplementaryInformationsDataMessage", CB_MapComplementaryInformation)
+end
+
+function IsMine(packet)
+    local elementIdOnMap = {}
+
+    for _, v in pairs(packet.statedElements) do
+        if v.onCurrentMap then
+            table.insert(elementIdOnMap, v.elementId)
+        end
+    end
+
+    for _, vIntegereractive in pairs(packet.integereractiveElements) do
+        local onMap = false
+
+        for _, elementId in pairs(elementIdOnMap) do
+            if vIntegereractive.elementId == elementId then
+                onMap = true
+                break
+            end
+        end
+
+        if onMap then
+            for _, v in pairs(mineurInfo) do
+                if vIntegereractive.elementTypeId == v.gatherId then
+                    return true
+                end
+            end
+        end
+    end
+    return false
+end
+
+function MineExist(mapsDecode)
+    for _, vArea in pairs(mapsDecode) do
+        if type(vArea) == "table" then
+            for kSubArea, vSubArea in pairs(vArea) do
+                if type(vSubArea) =="table" then
+                    for kMap, _ in pairs(vSubArea) do
+                        if Utils:Equal(map:currentMapId(), kMap) then
+                            return kSubArea
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return false
+end
+
+function GetLastMineId(mapsDecode)
+    local ret = 0
+    for _, vArea in pairs(mapsDecode) do
+        if type(vArea) == "table" then
+            for kSubArea,_ in pairs(vArea) do
+                if tonumber(kSubArea) ~= nil then
+                    if tonumber(kSubArea) > ret then
+                        ret = tonumber(kSubArea)
+                    end
+                end
+            end
+        end
+    end
+    return ret
 end
 
 function CB_MapComplementaryInformation(packet)
@@ -22,6 +103,8 @@ function CB_MapComplementaryInformation(packet)
 
         local currentMapId = map:currentMapId()
 
+        local subAreaId = tostring(packet.subAreaId)
+
         if mapsDecode[currentAreaId] == nil then
             newEntry = true
             Utils:Print("New Area entry !")
@@ -29,21 +112,38 @@ function CB_MapComplementaryInformation(packet)
             mapsDecode[currentAreaId].areaId = currentAreaId
         end
 
-        if mapsDecode[currentAreaId][tostring(packet.subAreaId)] == nil then
-            newEntry = true
-            Utils:Print("New SubArea entry !")
-            mapsDecode[currentAreaId][tostring(packet.subAreaId)] = {}
-            mapsDecode[currentAreaId][tostring(packet.subAreaId)].subAreaId = packet.subAreaId
+        if IsMine(packet) then
+            local alreadyExist = MineExist(mapsDecode)
+            inMine = true
+            if alreadyExist then
+                idMine = alreadyExist
+            elseif global:question("Nouvelle mine ?") then
+                idMine = GetLastMineId(mapsDecode) + 1
+            end
+            subAreaId = idMine .. ""
+        elseif inMine then
+            if global:question("Fin de la mine ?") then
+                inMine = false
+            else
+                subAreaId = idMine .. ""
+            end
         end
 
-        if mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""] == nil then
+        if mapsDecode[currentAreaId][subAreaId] == nil then
+            newEntry = true
+            Utils:Print("New SubArea entry !")
+            mapsDecode[currentAreaId][subAreaId] = {}
+            mapsDecode[currentAreaId][subAreaId].subAreaId = packet.subAreaId
+        end
+
+        if mapsDecode[currentAreaId][subAreaId][currentMapId .. ""] == nil then
             newEntry = true
             Utils:Print("New Map entry !")
-            mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""] = {}
-            mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""].mapId = currentMapId
-            mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""].posX = map:getX(currentMapId)
-            mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""].posY = map:getY(currentMapId)
-            mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""].gatherElements = {}
+            mapsDecode[currentAreaId][subAreaId][currentMapId .. ""] = {}
+            mapsDecode[currentAreaId][subAreaId][currentMapId .. ""].mapId = currentMapId
+            mapsDecode[currentAreaId][subAreaId][currentMapId .. ""].posX = map:getX(currentMapId)
+            mapsDecode[currentAreaId][subAreaId][currentMapId .. ""].posY = map:getY(currentMapId)
+            mapsDecode[currentAreaId][subAreaId][currentMapId .. ""].gatherElements = {}
 
             local elementIdOnMap = {}
 
@@ -64,12 +164,12 @@ function CB_MapComplementaryInformation(packet)
                 end
 
                 if onMap then
-                    if #mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""].gatherElements == 0 then
+                    if #mapsDecode[currentAreaId][subAreaId][currentMapId .. ""].gatherElements == 0 then
                         local gatherElement = { gatherId = vIntegereractive.elementTypeId, count = 1 }
-                        table.insert(mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""].gatherElements, gatherElement)
+                        table.insert(mapsDecode[currentAreaId][subAreaId][currentMapId .. ""].gatherElements, gatherElement)
                     else
                         local alreadyExist = false
-                        for _, v in pairs(mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""].gatherElements) do
+                        for _, v in pairs(mapsDecode[currentAreaId][subAreaId][currentMapId .. ""].gatherElements) do
                             if v.gatherId == vIntegereractive.elementTypeId then
                                 alreadyExist = true
                                 v.count = v.count + 1
@@ -79,7 +179,7 @@ function CB_MapComplementaryInformation(packet)
 
                         if not alreadyExist then
                             local gatherElement = { gatherId = vIntegereractive.elementTypeId, count = 1 }
-                            table.insert(mapsDecode[currentAreaId][tostring(packet.subAreaId)][currentMapId .. ""].gatherElements, gatherElement)    
+                            table.insert(mapsDecode[currentAreaId][subAreaId][currentMapId .. ""].gatherElements, gatherElement)    
                         end
                     end
                 end
